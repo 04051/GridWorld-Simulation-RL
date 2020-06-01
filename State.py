4bf182ba62db99a981fc956e4458788ce67bcedd
Kainat Abbasi <33037474+04051@users.noreply.github.com>
2020-06-01 16:34:19 -0400

    Updated State.py
    This file determines the state of a cell through the direction

---
+++ State.py
@@ -0,0 +1,17 @@
+class State:
+    def __init__(self, x, y, value, direction):
+        self.x = x
+        self.y = y
+        self.direction = direction  
+        self.reward = 0
+        self.value = value
+        self.isObstacle = False
+        self.dimX = 0
+        self.dimY = 0
+
+
+
+        
+        
+        
+        
